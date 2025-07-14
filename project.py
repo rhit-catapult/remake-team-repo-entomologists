4e53959bc096c379072c2a777838b8f39f900743
NaeNae4321 <debruler.nl@gmail.com>
2025-07-14 15:19:26 -0400

    added platforms

--- project.py
+++ project.py
@@ -13,7 +13,7 @@ def main():
     player = p.Player(100, 100, 32, 32)
 
     tile = level.Tile(0,screen)
-    tiles = [tile]*17 + [0]*14 + [tile]*2 + [0]*14 + [tile]*2 + [0]*14 + [tile]*2 + [0]*15 + [tile] + [0]*15 + [tile] + [0]*15 + [tile] + [0]*15 + [tile] * 17
+    tiles = [tile]*17 + [0]*14 + [tile]*2 + [0]*14 + [tile]*4 + [0]*12 + [tile]*2 + [0]*15 + [tile] + [0]*4 + [tile]*3+ [0]*8 + [tile] + [0]*15 + [tile] + [0]*15 + [tile] * 17
     room = level.Room(tiles)
     while True:
         clock.tick(60)
