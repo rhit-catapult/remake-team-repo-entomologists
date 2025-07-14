f673f0c71efcd3317135af714446604bd0ba3669
rhit-ulozasdj <davidulozas@gmail.com>
2025-07-14 11:49:46 -0400

    Added Collision
    Collisions do not work if bullet is a sprite.

--- bullet_program.py
+++ bullet_program.py
@@ -12,30 +12,48 @@ class Bullet:
         self.origin_x = x
         self.speed_x = speed_x
         self.speed_y = speed_y
-
+        # hitbox is here for init
+        self.hitbox = pygame.Rect(0,0,0,0)
 
     def draw(self):
         if self.image == 'placeholder':
             pygame.draw.line(self.screen,(255,100,100),(self.x,self.y),(self.x+25,self.y),5)
+            self.hitbox = pygame.Rect(self.x,self.y,25,5)
+            self.hitbox.move_ip(self.speed_x,self.speed_y)
         else:
             self.screen.blit(pygame.image.load(self.image),(self.x, self.y))
-            hitbox = pygame.image.load(self.image).get_rect()
+
+            self.hitbox = (pygame.image.load(self.image)).get_rect()
+            self.hitbox.move(self.speed_x,self.speed_y)
 
     def move(self):
         self.x += self.speed_x
         self.y += self.speed_y
 
+    def collision(self,rectangles):
+        for rectangle in rectangles:
+            if pygame.Rect.colliderect(self.hitbox, rectangle):
+                return True
+        else:
+            return False
 
-def bullet_update(bullets):
+def bullet_update(bullets,rectangles):
     for bullet in bullets:
+        remove_bullet = False
         bullet.move()
         bullet.draw()
+        if bullet.collision(rectangles):
+            remove_bullet = True
+            print("splat")
+
         if bullet.x > bullet.origin_x + 1000:
             print("pew")
+            remove_bullet = True
+        if remove_bullet:
             bullets.remove(bullet)
             del bullet
 
-        
+
 def debug():
     pygame.init()
     clock = pygame.time.Clock()
@@ -45,6 +63,9 @@ def debug():
     screen = pygame.display.set_mode((800,600))
     test_bullet = Bullet(screen,'Pistol.png')
     test_bullet2 = Bullet(screen,'placeholder',100,75,6)
+    test_rect = pygame.rect.Rect(400,0,300,600)
+    rectangles = []
+    rectangles.append(test_rect)
     bullets = []
     bullets.append(test_bullet)
     bullets.append(test_bullet2)
@@ -56,7 +77,8 @@ def debug():
             if event.type == pygame.QUIT:
                 sys.exit()
         screen.fill((255,255,255))
-        bullet_update(bullets)
+
+        bullet_update(bullets,rectangles)
         pygame.display.update()
 debug()
 
