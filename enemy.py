1a517168970b77127f78dfd949077f913ea7e02c
rhit-harmgc <harmgc@rose-hulman.edu>
2025-07-15 13:45:02 -0400

    Updated enemy.py

--- enemy.py
+++ enemy.py
@@ -1,5 +1,7 @@
 import pygame
 import sys
+
+import player
 import tiles
 pygame.init()
 bullet_damage = 100
@@ -10,15 +12,23 @@ class Walker:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.speedx = 100
+        self.speedx = 15
         self.health = 500
     def draw(self, screen):
-        pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, 32, 32))
-    def move(self, x, y):
+        if self.health <= 0:
+            return False
+        if True:
+            pygame.draw.rect(screen, (255, 0, 0), (self.x, self.y, 32, 32))
+        if False:
+            pass
+    def move(self):
         self.x = self.x + self.speedx
     def collide(self, tile):
-        self.speedx = -self.speedx
+        if pygame.Rect.colliderect(tile, self):
+            self.speedx = -self.speedx
     def hurt(self, player):
-        player.health = player.health - enemy_damage
+        if pygame.Rect.colliderect(player, self):
+            player.health = player.health - enemy_damage
     def get_hurt(self):
-        self.health = self.health - bullet_damage
+        if pygame.Rect.colliderect(self, bullet):
+            self.health = self.health - bullet_damage
