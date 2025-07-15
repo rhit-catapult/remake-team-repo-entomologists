c5942b2259fc8cccf1f92ba4fd64b0483b8c3667
NaeNae4321 <debruler.nl@gmail.com>
2025-07-15 15:49:17 -0400

    worked on enemies

--- project.py
+++ project.py
@@ -48,10 +48,12 @@ def main():
     gun = weapons.Gun(screen, 0, 0, "Pistol.png", 20, 100)
 
     tile = level.Tile(0,screen)
-    tiles1 = [tile]*17 + [0]*14 + [tile]*2 + [0]*14 + [tile]*4 + [0]*12 + [tile]*2 + [0]*15 + [tile] + [0]*4 + [tile]*3+ [0]*8 + [tile] + [0]*15 + [tile] + [0]*15 + [tile] * 17
-    tiles2 = [tile]*17 + [0]*14 + [tile]*2 + [0]*14 + [tile]*2 + [0]*14 + [tile] + [0]*15 + [tile] + [0]*16 + [0]*16 + [0]*16 + [tile]*16
-    tiles3 = [tile]*13 + [0]*2 + [tile]*2 + [0]*14 + [tile]*2 + [0]*12 + [tile]*4 + [0]*14 + [tile]*2 + [0]*14 + [tile]*1 + [0]*7 + [tile]*2 + [0]*6 + [tile]*1 + [0]*15 + [tile]*1 + [0]*15 + [tile]*17
-    room = [level.Room(tiles1, 0, 0), level.Room(tiles2, 1, 0), level.Room(tiles3, 2, 0)]
+    tiles1 = [tile] * 17 + [0] * 14 + [tile] * 2 + [0] * 14 + [tile] * 4 + [0] * 12 + [tile] * 2 + [0] * 15 + [tile] + [0] * 4 + [tile] * 3 + [0] * 8 + [tile] + [0] * 15 + [tile] + [0] * 15 + [tile] * 17
+    tiles2 = [tile] * 17 + [0] * 14 + [tile] * 2 + [0] * 14 + [tile] * 2 + [0] * 14 + [tile] + [0] * 15 + [tile] + [0] * 16 + [0] * 16 + [0] * 16 + [tile] * 16
+    tiles3 = [tile] * 13 + [0] * 2 + [tile] * 2 + [0] * 14 + [tile] * 2 + [0] * 12 + [tile] * 4 + [0] * 14 + [tile] * 2 + [0] * 14 + [tile] * 1 + [0] * 7 + [tile] * 2 + [0] * 6 + [tile] * 1 + [0] * 15 + [tile] * 1 + [0] * 15 + [tile] * 17
+    tiles4 = [tile] * 17 + [0] * 15 + [tile] + [0] * 15 + [tile] * 1 + [0] * 15 + [tile] + [0] * 5 + [tile] * 11 + [0] * 14 + [tile] * 5 + [0] * 11 + [tile] * 5 + [0] * 11 + [tile] * 14 + [0] * 2 + [tile]
+    tiles5 = [tile] * 16 + [0] * 15 + [tile] + [0] * 15 + [tile] + [0] * 15 + [tile] * 11 + [0] * 2 + [tile] * 5 + [0] * 14 + [tile] * 2 + [0] * 15 + [tile] + [0] * 15 + [tile] * 16
+    room = [level.Room(tiles1, 0, 0), level.Room(tiles2, 1, 0), level.Room(tiles3, 2, 0), level.Room(tiles4, 2, -1),level.Room(tiles5, 3, -1)]
     l = level.Level(room)
 
     x_offset = 0
