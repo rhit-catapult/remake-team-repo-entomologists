96f94249523021b2a7092e05c997a06bdaf812fb
rhit-harmgc <harmgc@rose-hulman.edu>
2025-07-16 10:08:16 -0400

    Update project_level.py

--- project_level.py
+++ project_level.py
@@ -53,7 +53,7 @@ def main():
     screen = pygame.display.set_mode((SCREENWIDTH, SCREENHEIGHT))
     clock = pygame.time.Clock()
 
-    player = p.Player(100, 100, 32, 32)
+    player = p.Player(4200, -630, 32, 32)
 
     tile = level.Tile(0,screen)
     tile2 = level.Tile(1,screen)
@@ -63,7 +63,13 @@ def main():
     tiles4 = [tile]*17 + [0]*15 + [tile] + [0]*15 + [tile]*1 + [0]*15 + [tile] + [0]*5 + [tile]*11 + [0]*14 + [tile]*5 + [0]*11 + [tile]*5 + [0]*11 + [tile]*14 + [0]*2+[tile]
     tiles5 = [tile]*16 + [0]*15 + [tile] + [0]*15 + [tile] + [0]*15 + [tile]*12 + [0]*4 + [tile]*2 + [0]*13 + [tile]*3 + [0]*12 + [tile]*3 + [tile] + [0]*11 + [tile]*6 + [0]*2 + [tile]*12
     tiles6 = [tile]*2 + [0]*2 + [tile]*13 + [0]*14 + [tile]*2 + [0]*14 + [tile]*2 + [0] + [tile]*2 + [0]*11 + [tile]*2 + [0]*5 + [tile]*2 +[0]*7 +[tile]*2 + [0]*14 + [tile]*2 + [0]*9 +[tile]*2 +[0]*4 +[tile] + [0]*15 + [tile] + [tile2]*12 + [tile]*3
-    room = [level.Room(tiles1, 0, 0), level.Room(tiles2, 1, 0), level.Room(tiles3, 2, 0), level.Room(tiles4, 2, -1), level.Room(tiles5, 3, -1), level.Room(tiles6, 3, 0)]
+    tiles7 = [tile]*13 + [0]*2 + [tile]*2 + [0]*14 + [tile]*2 + [0]*12 + [tile]*4 + [0]*14 + [tile]*2 + [0]*6+[tile]*3 + [tile]*2 +[0]*3 + [tile]*2 + [0]*14+ [tile] + [0]*4 + [tile]*2 +[0]*9+ [tile] + [0]*15 + [tile]*4 + [tile2]*12 + [tile]
+    tiles8 = [tile]*13 + [0]*2 + [tile]*2 + [0]*14 + [tile]*2 + [0]*13 + [tile]*3 + [0]*6 +[tile] + [0]*2 +[tile] + [0]*2 +[tile]*2 + [tile]*2 + [0]*3 + [tile] + [tile2]*8 + [tile]*5 + [0]*2 + [tile]*13 + [0]*14 + [tile]*2 + [0]*14 + [tile]*14 + [0]*2 + [tile]
+    tiles9 = [tile]*17 + [0]*14 + [tile]*2 + [0]*14 + [tile]*2 + [0]*4 + [tile]*6 + [0]*4 + [tile]*2 + [0]*6 + [tile]*2 + [0]*6 + [tile]*5 + [0]*3 + [tile]*2 + [0]*3 + [tile]*4 + [0]*7 + [tile]*2 + [0]*6 + [tile] + [0]*7 + [tile]*2 + [0]*6 + [tile]*14 + [0]*2 +[tile]
+    tiles10 = [tile]*16 + [tile]*5 + [0]*6 + [tile]*5 + [tile]*3 + [0]*10 +[tile]*3 + [tile]*2 + [0]*12 + [tile]*2 +[tile]+[0]*14 +[tile] +[tile] +[0]*14 +[tile]*2+ [0]*15 +[tile] +[0]*15 + [tile]*16
+    #tiles11 = [tile]*17 + [0]*14 + [tile]*2+ [0]*14 + [tile]*2+ [0]*14 + [tile]*2+ [0]*14 + [tile]*2+ [0]*14 + [tile]*2+ [0]*15 + [tile]+ [0]*15 + [tile]*16
+
+    room = [level.Room(tiles1, 0, 0), level.Room(tiles2, 1, 0), level.Room(tiles3, 2, 0), level.Room(tiles4, 2, -1), level.Room(tiles5, 3, -1), level.Room(tiles6, 3, 0), level.Room(tiles7, 4, 0), level.Room(tiles8, 4, -1), level.Room(tiles9, 4, -2), level.Room(tiles10, 3, -2)]
     l = level.Level(room)
 
     x_offset = 0
